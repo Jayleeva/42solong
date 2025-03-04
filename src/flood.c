cbca259a9b1d8614fb9cdf1e83cbf1fb509332d7
Cynthia Glardon <cyglardo@c1r13s7.42lausanne.ch>
2025-03-04 15:33:33 +0100

    flood a revoir

--- src/flood.c
+++ src/flood.c
@@ -10,15 +10,12 @@ int	is_in_set(char c)
 	set[1] = 'C';
 	set[2] = 'E';
 	set[3] = 'P';
+	//set[4] = 'F';
 	i = 0;
 	while (set[i])
 	{
 		if (set[i] == c)
-		{
-			write(1, &c, 1);
-			write(1, "\n", 1);
 			return (1);
-		}
 		else
 			i ++;
 	}
@@ -27,35 +24,29 @@ int	is_in_set(char c)
 
 char	**flood(char **tab, t_point size, t_point begin)
 {
-	char	c;
 	t_point	p;
 
-	c = tab[begin.y][begin.x];
 	tab[begin.y][begin.x] = 'F';
-	if (begin.y > 0 && tab[begin.y - 1][begin.x] == is_in_set(c))
+	if (begin.y > 0 && is_in_set(tab[begin.y - 1][begin.x]))
 	{
-		write(1, "DOWN\n", 5);
 		p.x = begin.x;
 		p.y = begin.y - 1;
 		flood(tab, size, p);
 	}
-	if ((begin.y < (size.y - 1)) && tab[begin.y + 1][begin.x] == is_in_set(c))
+	if ((begin.y < (size.y - 1)) && is_in_set(tab[begin.y + 1][begin.x]))
 	{
-		write(1, "UP\n", 3);
 		p.x = begin.x;
 		p.y = begin.y + 1;
 		flood(tab, size, p);
 	}
-	if ((begin.x < (size.x - 1)) && tab[begin.y][begin.x + 1] == is_in_set(c))
+	if ((begin.x < (size.x - 1)) && is_in_set(tab[begin.y][begin.x + 1]))
 	{
-		write(1, "RIGHT\n", 6);
 		p.x = begin.x + 1;
 		p.y = begin.y;
 		flood(tab, size, p);
 	}
-	if (begin.x > 0 && tab[begin.y][begin.x - 1] == is_in_set(c))
+	if (begin.x > 0 && is_in_set(tab[begin.y][begin.x - 1]))
 	{
-		write(1, "LEFT\n", 5);
 		p.x = begin.x - 1;
 		p.y = begin.y;
 		flood(tab, size, p);
