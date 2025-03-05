18579090aaccf4cd8590a35a9003c1cf121ab7ad
orifcg <cynthia.glardon@formation.orif.ch>
2025-03-05 10:10:57 +0000

    windooow

--- src/create_window.c
+++ src/create_window.c
@@ -3,32 +3,23 @@
 
 int	initialize(char **tab)
 {
-	void    *mlx_ptr;
-	void    *win_ptr;
+	t_data data;
 
 	(void)tab;
-	/*t_data data;
-
 	data.mlx_ptr = mlx_init();
 	if (!data.mlx_ptr)
+	{
+		write(1, "init\n", 5);
 		return (1);
+	}
 	data.win_ptr = mlx_new_window(data.mlx_ptr, 600, 400, "hi :)");
 	if (!data.win_ptr)
+	{
+		write(1, "window\n", 7);
 		return (free(data.mlx_ptr), 1);
-
-	mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &on_keypress, &data);
-	mlx_hook(data.win_ptr, DestroyNotify, StructureNotifyMask, &on_destroy, &data);
+	}
+	//mlx_hook(data.win_ptr, KeyRelease, KeyReleaseMask, &on_keypress, &data);
+	//mlx_hook(data.win_ptr, DestroyNotify, StructureNotifyMask, &on_destroy, &data);
 	mlx_loop(data.mlx_ptr);
-	return (0);*/
-
-	mlx_ptr = mlx_init();
-	if (!mlx_ptr)
-		return (1);
-    win_ptr = mlx_new_window(mlx_ptr, 600, 400, "hi :)");
-    if (!win_ptr)
-        return (free(mlx_ptr), 1);
-    mlx_destroy_window(mlx_ptr, win_ptr);
-    mlx_destroy_display(mlx_ptr);
-    free(mlx_ptr);
 	return (0);
 }
