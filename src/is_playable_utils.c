9648c95722ca2b9d9419f9e1fd4efc1a766e0988
Jayleeva <101644976+Jayleeva@users.noreply.github.com>
2025-03-12 14:16:32 +0100

    Update is_playable_utils.c

--- src/is_playable_utils.c
+++ src/is_playable_utils.c
@@ -56,3 +56,19 @@ int	is_everything_flooded(char **map)
 	}
 	return (1);
 }
+
+char	**flood(char **tab, t_point size, t_point begin)
+{
+	tab[begin.x][begin.y] = 'F';
+	if (begin.y > 0 && is_in_set(FLOOD_SET, tab[begin.x][begin.y - 1]))
+		flood(tab, size, (t_point){begin.x, begin.y -1});
+	if ((begin.y < (size.y - 1))
+		&& is_in_set(FLOOD_SET, tab[begin.x][begin.y + 1]))
+		flood(tab, size, (t_point){begin.x, begin.y +1});
+	if ((begin.x < (size.x - 1))
+		&& is_in_set(FLOOD_SET, tab[begin.x + 1][begin.y]))
+		flood(tab, size, (t_point){begin.x +1, begin.y});
+	if (begin.x > 0 && is_in_set(FLOOD_SET, tab[begin.x - 1][begin.y]))
+		flood(tab, size, (t_point){begin.x -1, begin.y});
+	return (tab);
+}
