5c8d4d80b552bdeb1b4baca52b46fd23f16c925e
Jayleeva <101644976+Jayleeva@users.noreply.github.com>
2025-03-12 14:02:44 +0100

    Update update_tiles.c

--- src/update_tiles.c
+++ src/update_tiles.c
@@ -38,7 +38,6 @@ void	update_last_tile(t_data *data, t_point pos)
 			data->tiles.ground.img, pos.y * 64, pos.x * 64);
 	}
 }
-
 void	update_new_tile(t_data *data, t_point pos)
 {
 	if (data->map[pos.x][pos.y] == 'C')
@@ -46,21 +45,13 @@ void	update_new_tile(t_data *data, t_point pos)
 		data->map[pos.x][pos.y] = 'c';
 		data->c_remaining --;
 	}
-	if (data->was_carot == 1 || data->map[pos.x][pos.y] == 'c')
-	{
+	if (data->map[pos.x][pos.y] == 'c')
 		data->was_carot = 1;
-		mlx_put_image_to_window(data->mlx_ptr, data->win_ptr,
-			data->tiles.collected.img, pos.y * 64, pos.x * 64);
-	}
 	else if (data->map[pos.x][pos.y] == 'E')
 	{
 		if (data->c_remaining == 0)
 			on_destroy(data);
 		else
-		{
 			data->was_exit = 1;
-			mlx_put_image_to_window(data->mlx_ptr, data->win_ptr,
-				data->tiles.exit.img, pos.y * 64, pos.x * 64);
-		}
 	}
 }
